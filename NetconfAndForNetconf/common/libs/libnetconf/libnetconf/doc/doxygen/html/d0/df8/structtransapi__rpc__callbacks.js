bdc24f3c655987dde415088915acd273df9b693e
Dmitriy Semykin <dmitriy.semykin@eltex.loc>
2016-03-09 15:35:34 +0600

    Добавил исходники libnetconf

---
+++ NetconfAndForNetconf/common/libs/libnetconf/libnetconf/doc/doxygen/html/d0/df8/structtransapi__rpc__callbacks.js
@@ -0,0 +1,7 @@
+var structtransapi__rpc__callbacks =
+[
+    [ "callbacks", "d0/df8/structtransapi__rpc__callbacks.html#a0ad28c1d0d3518ca0b3d631a20eaf48d", null ],
+    [ "callbacks_count", "d0/df8/structtransapi__rpc__callbacks.html#a853d73f0f495147dba4842e9f860d1f0", null ],
+    [ "func", "d0/df8/structtransapi__rpc__callbacks.html#a4e198e0f385cc9d713aa918597858fbb", null ],
+    [ "name", "d0/df8/structtransapi__rpc__callbacks.html#a5ac083a645d964373f022d03df4849c8", null ]
+];
---
+++ NetconfAndForNetconf/source/libnetconf/doc/doxygen/html/d0/dd1/structclbk.js
@@ -0,0 +1,5 @@
+var structclbk =
+[
+    [ "func", "d0/dd1/structclbk.html#aecdb0401379b34c8aa71dd594b79d45d", null ],
+    [ "path", "d0/dd1/structclbk.html#a44196e6a5696d10442c29e639437196e", null ]
+];
