e53e3ca515109128d0991f5d529aa3aa8e6df845
Dmitriy <semyda94@yandex.ru>
2016-03-14 20:01:46 +0600

    Add .gitignore in root make and begin in first steps

--- NetconfAndForNetconf/sim/config.mk
+++
@@ -1,3 +0,0 @@
-LIBS_DIR    = /Users/SemykinDmitriy/Desktop/Diplom/NetconfAndForNetconf/sim/../common/libs
-INCLUDE_DIR = /Users/SemykinDmitriy/Desktop/Diplom/NetconfAndForNetconf/sim/../common/include
-BOARD_NAME  = sim
